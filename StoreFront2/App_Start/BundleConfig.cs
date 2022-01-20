971caf616c09e1980c12511d339296e56360d0fb
ERIC Weibley <weibleyeric@yahoo.com>
2022-01-20 16:05:50 -0600

    added datalayer

--- StoreFront2/App_Start/BundleConfig.cs
+++ StoreFront2/App_Start/BundleConfig.cs
@@ -7,24 +7,35 @@ namespace StoreFront2
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Content/js/jquery-3.3.1.min.js",
+                      "~/Content/js/jquery-ui.js", 
+                      "~/Content/js/popper.min.js",
+                      "~/Content/js/bootstrap.min.js", 
+                      "~/Content/js/owl.carousel.min.js", 
+                      "~/Content/js/jquery.magnific-popup.min.js", 
+                      "~/Content/js/aos.js",
+                      "~/Content/js/main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/magnific-popup.css",
+                      "~/Content/css/jquery-ui.css",
+                      "~/Content/css/owl.carousel.min.css",
+                      "~/Content/css/owl.theme.default.min.css",
+                      "~/Content/css/aos.css",
+                      "~/Content/css/style.css"));
         }
     }
 }
