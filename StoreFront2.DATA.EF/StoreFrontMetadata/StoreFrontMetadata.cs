353a20d4b25b55b2405e0bee111065eed109813b
ERIC Weibley <weibleyeric@yahoo.com>
2022-01-22 14:28:46 -0600

    added metadata?

---
+++ StoreFront2.DATA.EF/StoreFrontMetadata/StoreFrontMetadata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations; 
+
+namespace StoreFront2.DATA.EF//.StoreFrontMetadata
+{
+    [MetadataType(typeof(ProductMetadata))]
+   public partial class Product
+   { }
+        
+    
+
+    public class ProductMetadata
+    {
+       [StringLength(50)]
+       [Display(Name = "Product Name")]
+       [Required]
+       public string Name { get; set; }
+
+        [StringLength(100)]
+        public string Description { get; set; }
+        public int CategoryID { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int ProductStatusID { get; set; }
+
+        [Required]
+        public Nullable<int> Quanity { get; set; }
+
+    }
+
+}
